bf2160e28df3b5db595912b13e3b655d606aab36
Hammy <hcj.sandall@hotmail.com>
2019-08-20 15:08:17 +1200

    translator 1

---
+++ main/translator.py
@@ -0,0 +1,9 @@
+#Requests
+def get():
+    #player
+    if request = 'name':
+        output == '{}'.format(playerName)
+    elif request = 'coords':
+        output == '{}'.format(playerXYZ)
+    elif request = 'health':
+        output == '{}'.format(playerHealth)
